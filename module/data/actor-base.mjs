9e559429b9202178b9dba4726f0d050194185789
Sparky939 <m.bower91@gmail.com>
2024-09-13 21:08:19 +0100

    Data Modal & remove node_modules from git

--- module/data/actor-base.mjs
+++ module/data/actor-base.mjs
@@ -2,15 +2,14 @@ export default class GodboundActorBase extends foundry.abstract.TypeDataModel {
 
   static defineSchema() {
     const fields = foundry.data.fields;
-    const requiredInteger = { required: true, nullable: false, integer: true };
+    // const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = {};
 
-    schema.resources = new fields.SchemaField({
-      effort: new fields.SchemaField({
-        value: new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 }),
-        max: new fields.NumberField({ ...requiredInteger, initial: 1 })
-      }),
-    })
+    // schema.resources = new fields.SchemaField({
+    //   effort: new fields.SchemaField({
+    //     value: new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 })
+    //   }),
+    // })
     schema.biography = new fields.StringField({ required: true, blank: true }); // equivalent to passing ({initial: ""}) for StringFields
 
     return schema;
--- module/data/actor-npc.mjs
+++ module/data/actor-npc.mjs
@@ -1,5 +1,22 @@
 import GodboundActorBase from "./base-actor.mjs";
 
+/**
+ * @param {object} resources
+ * @param {object} resources.hd
+ * @param {number} resources.hd.value
+ * @param {number} resources.hd.max
+ * @param {object} resources.effort
+ * @param {number} resources.effort.value
+ * @param {number} resources.effort.max
+ * @param {number} attributes.highSave
+ * @param {number} attributes.lowSave
+ * @param {number} attributes.ac
+ * @param {string} attributes.move
+ * @param {object} details
+ * @param {string} details.description
+ * @param {string} details.goal
+ */
+
 export default class GodboundNPC extends GodboundActorBase {
 
   static defineSchema() {
@@ -7,13 +24,41 @@ export default class GodboundNPC extends GodboundActorBase {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
-    schema.cr = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
-    schema.xp = new fields.NumberField({ ...requiredInteger, initial: 0, min: 0 });
-    
+    schema.resources = new fields.SchemaField({
+      hd: new fields.SchemaField({
+        value: new fields.NumberField({ ...requiredInteger, initial: 0 }),
+        max: new fields.NumberField({ ...requiredInteger, initial: 0 }),
+      }),
+      effort: new fields.SchemaField({
+        value: new fields.NumberField({ ...requiredInteger, initial: 0 }),
+        max: new fields.NumberField({ ...requiredInteger, initial: 0 }),
+      }),
+    });
+
+    schema.attributes = new fields.SchemaField({
+      highSave: new fields.NumberField({ ...requiredInteger, initial: 0 }),
+      lowSave: new fields.NumberField({ ...requiredInteger, initial: 0 }),
+      ac: new fields.NumberField({ ...requiredInteger, initial: 0 }),
+      move: new fields.StringField({ initial: "30 ft." }),
+    });
+
+    schema.details = new fields.SchemaField({
+      description: new fields.StringField({ initial: "" }),
+      goal: new fields.StringField({ initial: "" }),
+    })
+
     return schema
   }
 
-  prepareDerivedData() {
-    this.xp = this.cr * this.cr * 100;
+  prepareDerivedData() {}
+
+  getRollData() {
+    const data = {};
+
+    data.highsave = this.data.attributes.highSave;
+    data.lowsave = this.data.attributes.lowSave;
+    data.ac = this.data.attributes.ac;
+
+    return data;
   }
 }
